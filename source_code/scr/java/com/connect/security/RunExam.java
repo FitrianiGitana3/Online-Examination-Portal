07f0750b47fb8a7b75db7111df45a8f0027c0928
Samuel <67509713+samnunel56@users.noreply.github.com>
2020-07-18 01:04:59 +0700

    Update RunExam.java

--- source_code/scr/java/com/connect/security/RunExam.java
+++ source_code/scr/java/com/connect/security/RunExam.java
@@ -120,4 +120,16 @@ if(Direction.equals("Next"))
             out.close();
         }
     }
-
+   // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
