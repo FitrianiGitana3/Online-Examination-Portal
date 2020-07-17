ffc24767fcb74c3e9189bbaf63734f22bf5020a2
Rolay <vinny.sihombing@yahoo.com>
2020-07-18 00:20:04 +0700

    Update

--- source_code/scr/java/com/connect/security/RegisterNewUser.java
+++ source_code/scr/java/com/connect/security/RegisterNewUser.java
@@ -10,3 +10,19 @@ import java.sql.DriverManager;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+
+
+
+int output = insertUserInfo.executeUpdate();
+                
+                
+        //--------------------------------------------------------------------------------
+        // The following segment of out.println() code is only for debugging purposes...
+        //--------------------------------------------------------------------------------
+        out.println("<br><br><h3>New User Registration Details</h3>" 
+                + "-----------------------------------------------------------------<br>");
+        out.println("<br>Name     &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: "+Name);
+        out.println("<br>Email    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: "+Email);
+        out.println("<br>Username &nbsp&nbsp&nbsp&nbsp&nbsp: "+UserName);
+        out.println("<br>Password &nbsp&nbsp&nbsp&nbsp&nbsp: "+Password);
