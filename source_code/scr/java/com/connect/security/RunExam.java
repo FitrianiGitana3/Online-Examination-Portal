7f1db8528f6934e377257961160ec13c60b40b3a
Samuel <67509713+samnunel56@users.noreply.github.com>
2020-07-18 01:02:12 +0700

    Create RunExam.java

---
+++ source_code/scr/java/com/connect/security/RunExam.java
@@ -0,0 +1,11 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.connect.security;
+
+import java.sql.Statement;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
