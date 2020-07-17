be03cfe5fafae86e70786d63620b3ed1b816e11d
Rolay <vinny.sihombing@yahoo.com>
2020-07-18 00:18:48 +0700

    Update

--- source_code/scr/java/com/connect/security/CreateDownloadableTestReport.java
+++ source_code/scr/java/com/connect/security/CreateDownloadableTestReport.java
@@ -11,3 +11,29 @@ import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import java.io.ByteArrayOutputStream;
 
+
+
+        // step 4
+            Paragraph para1 = new Paragraph("ONLINE EXAMINATION PORTAL", catFont);
+            para1.setAlignment(Element.ALIGN_CENTER);
+            
+            Paragraph para2 = new Paragraph("This is to certify that", subFont);
+            para2.setAlignment(Element.ALIGN_CENTER);
+            
+            Paragraph newLine = new Paragraph(" ");
+            
+            Paragraph UserNameLine = new Paragraph(UserSession.getAttribute("Username").toString().toUpperCase(),catFont);
+            UserNameLine.setAlignment(Element.ALIGN_CENTER);
+            
+            Paragraph para3 = new Paragraph("has completed the exam titled", subFont);
+            para3.setAlignment(Element.ALIGN_CENTER);
+            
+            Paragraph ExamNameLine = new Paragraph(UserSession.getAttribute("ExamName").toString(),catFont);
+            ExamNameLine.setAlignment(Element.ALIGN_CENTER);
+            
+            Paragraph ExamIDLine = new Paragraph("(UEID : "+UserSession.getAttribute("UEID").toString()+")",subFont);
+            ExamIDLine.setAlignment(Element.ALIGN_CENTER);
+            
+            Paragraph para4 = new Paragraph("secured a score of", subFont);
+            para4.setAlignment(Element.ALIGN_CENTER);
+
