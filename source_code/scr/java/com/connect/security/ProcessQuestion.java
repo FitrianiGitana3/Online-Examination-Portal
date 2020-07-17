7f504a16301fa1560d0b1f66dd95792d054d6c7b
Rolay <vinny.sihombing@yahoo.com>
2020-07-18 00:22:26 +0700

    Update

--- source_code/scr/java/com/connect/security/ProcessQuestion.java
+++ source_code/scr/java/com/connect/security/ProcessQuestion.java
@@ -50,3 +50,15 @@ String query = "";
                 txtOptionB = convertTextareaContent(txtOptionB);
                 
                 Connection con = DriverManager.getConnection (dbUrl,"","");
+        
+        
+      /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
+
