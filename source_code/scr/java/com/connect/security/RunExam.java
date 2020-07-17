cfc8d3a77fc2866592379a0fe3adfb969b9902b2
Nicodemus <67509481+nic2531@users.noreply.github.com>
2020-07-18 01:08:56 +0700

    Update RunExam.java

--- source_code/scr/java/com/connect/security/RunExam.java
+++ source_code/scr/java/com/connect/security/RunExam.java
@@ -1,16 +1,1 @@
-     
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-    
-     /** 
-     * Returns a short description of the servlet.
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-}
+
