45b222f3d9c388c56b439785017356ace829fe86
Rolay <vinny.sihombing@yahoo.com>
2020-07-18 00:12:26 +0700

    Update

---
+++ source_code/scr/java/com/connect/security/ChangeUserPrivilage.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.connect.security;
+
+import java.io.IOException;
+import java.sql.Connection;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Devanand
+ */
+public class ChangeUserPrivilage extends HttpServlet {
