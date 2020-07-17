82d2e0455070bae6b6328bb644201bf847286b29
Masni <47467309+Masnika@users.noreply.github.com>
2020-07-18 00:31:52 +0700

    update

--- source_code/scr/java/com/connect/security/RegisterNewUser.java
+++ source_code/scr/java/com/connect/security/RegisterNewUser.java
@@ -13,6 +13,25 @@ import javax.servlet.http.HttpServletResponse;
 
 
 
+protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Please wait while we analyze your request...</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h2>Please wait... </h2>");
+           // out.println("<br><h3>Oops... it seems our registration service is currently being updated...<br>"
+             //       + "We're sorry for the inconvenience.... "
+               //     + "You can check back later and we'll have it all up and running again.... Thank You..!!!</h3>");
+            
+            
+        Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+
+
 
 int output = insertUserInfo.executeUpdate();
                 
