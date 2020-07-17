f85e198e38300e7a3efa71117adfad2c8156b19c
Rolay <vinny.sihombing@yahoo.com>
2020-07-18 00:15:25 +0700

    Update

--- source_code/scr/java/com/connect/security/ChangeUserPrivilage.java
+++ source_code/scr/java/com/connect/security/ChangeUserPrivilage.java
@@ -14,3 +14,29 @@ import javax.servlet.http.HttpServletResponse;
  * @author Devanand
  */
 public class ChangeUserPrivilage extends HttpServlet {
+
+  String query = "";
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String UserIdToChange = "";
+        String ChangeType = "";
+        String AddAdminQuery = "";
+        String RemoveAdminQuery = "";
+        int output;
+        HttpSession UserSession = request.getSession(false);
+        try {
+            UserIdToChange = request.getParameter("UserToChange");
+            ChangeType = request.getParameter("ChangeType");
+            
+            //out.print(UserIdToChange + " " + ChangeType);
+            
+            Connection con = DriverManager.getConnection (dbUrl,"","");
+        PreparedStatement userPrivilageChanger;
+        
+        if("ToAdminUser".equals(ChangeType))
+        {
+        AddAdminQuery="insert into Administrator_List (UserId) values(?)";
+        output = userPrivilageChanger.executeUpdate();
+
