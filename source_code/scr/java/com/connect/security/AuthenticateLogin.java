76ea9f81817a845c99585c5b1b2349bbe74e4f2c
Rolay <vinny.sihombing@yahoo.com>
2020-07-18 00:23:08 +0700

    Create AuthenticateLogin.java

---
+++ source_code/scr/java/com/connect/security/AuthenticateLogin.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.connect.security;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.sql.*;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Devanand
+ */
